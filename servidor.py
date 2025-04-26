ea0dd0815d13e177735c9fa5a1bf21f4db6a3f0e
Anna Eliza <annaeliza.als@gmail.com>
2025-04-26 15:48:24 -0300

    update

---
+++ servidor.py
@@ -0,0 +1,22 @@
+import socket #biblioteca 
+
+servidor = socket.socket(socket.AF_INET, socket.socket.SOCK_STREAM)
+servidor.bind(('localhost', 8888))
+
+servidor.listen()
+cliente, end = servidor.accept()
+
+terminado = False
+
+print('Chat iniciado!')
+
+while not terminado:
+    msg = cliente.recv(1024).encode('utf-8')
+
+    if msg == 'sair':
+        terminado = True
+    else:
+        cliente.send(input('Mensagem: ').encode('utf-8'))
+
+servidor.close()
+cliente.close()
