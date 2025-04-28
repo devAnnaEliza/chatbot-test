aac008b883c9c6ac9718f8db4dae970650311818
Anna Eliza <annaeliza.als@gmail.com>
2025-04-28 15:29:12 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,8 +4,10 @@ servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 servidor.bind(('localhost', 8888))
 
 servidor.listen(1)
+print("Aguardando conexão...")
 
 conexao, endereco = servidor.accept()
+print(f"Conexão estabelecida com {endereco}")
 
 terminado = False
 
@@ -13,11 +15,14 @@ print('Chat iniciado!')
 
 while not terminado:
     msg = cliente.recv(1024).decode('utf-8')
+    print(f"Mensagem recebida: {msg}")
 
     if msg == 'sair':
         terminado = True
+        conexao.send('sair'.encode('utf-8'))
     else:
-        cliente.send(input('Mensagem: ').encode('utf-8'))
+        resposta = f"Você disse: {msg}"
+        cliente.send(resposta.encode('utf-8'))
 
 conexao.close()
 servidor.close()
