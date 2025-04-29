af5779f3a5a5b1236182d8c796a162e37263bf2d
Anna Eliza <annaeliza.als@gmail.com>
2025-04-29 18:37:14 -0300

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -2,16 +2,16 @@ import socket
 import threading
 
 def handle_client(conexao, endereco):
-    print(f"Conexaõ estabelecida com {endereco}")
+    print(f"Conexão estabelecida com {endereco}")
     while True:
         try:
-        msg = cliente.recv(1024).decode('utf-8')
+            msg = cliente.recv(1024).decode('utf-8')
         if msg == 'sair':
             print(f"Conexão encerrada com {endereco}")
             conexao.send('sair'.encode('utf-8'))
             break
         else:
-            resposta = f"Você disse: {msg}
+            resposta = f"Você disse: {msg}"
             conexao.send(resposta.encode('utf-8'))
         except:
             break
