1a2c31b93c2fa99abaaadc696b5f0586c40bf79d
Anna Eliza <annaeliza.als@gmail.com>
2025-04-28 21:22:34 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,20 +1,26 @@
 import socket
 
-cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-cliente.connect(('localhost', 8888))
+try:
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    cliente.connect(('localhost', 8888))
 
 terminado = False
 
 print('Digite "sair" para encerrar')
 
 while not terminado:
-    cliente.send(input('Mensagem:').encode('utf-8'))
-    msg = cliente.recv(1024).decode('utf-8')
+    try:
+        cliente.send(input('Mensagem:').encode('utf-8'))
+        msg = cliente.recv(1024).decode('utf-8')
 
     if msg == 'sair':
         terminado = True
 
     else:
         print(msg)
+    except ConnectionAbortedError:
+        print("Conexão encerrada pelo servidor.")
+    except Exception as e:
+        print(f"Erro inesperado: {e}")
 
 cliente.close()
